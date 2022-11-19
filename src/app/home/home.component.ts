9757d6bb60bace0283311558620d9bf6cbf91888
Tom Bachant <bachonk@gmail.com>
2022-11-19 13:14:00 -0500

    add some basic stuff

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit {
   }
   ngOnInit(): void {
     var options = {
-      strings: ['','Full-Stack', 'WEB','Mobile'],
+      strings: ['','fossil-fuel burning machines', 'pedestrian-killing machines', ''],
       typeSpeed: 120,
       backSpeed: 100,
       loop: true,
